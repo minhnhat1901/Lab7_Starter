2a2b4145218f0e4850f0e24f19b543cda2f8a4db
Minh Duong <64678096+minhnhat1901@users.noreply.github.com>
2023-05-21 00:52:26 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -68,24 +68,23 @@ self.addEventListener('fetch', function (event) {
       });
     }));
   } else {
-    return;
-    // event.respondWith(caches.open(CACHE_NAME).then(async (cache) => {
-    //   // Respond with the resource from the cache or from the network
-    //   return cache.match(event.request).then((cachedResponse) => {
-    //     // Return a cached response if we have one
-    //     if (cachedResponse) {
-    //       return cachedResponse;
-    //     }
-    //     else {
-    //       return fetch(event.request).then((fetchedResponse) => {
-    //         // Add the network response to the cache for later visits
-    //         cache.put(event.request, fetchedResponse.clone());
-  
-    //         // Return the network response
-    //         return fetchedResponse;
-    //       });
-    //     }
-    //   });
-    // }));
+    event.respondWith(caches.open(CACHE_NAME).then(async (cache) => {
+      // Respond with the resource from the cache or from the network
+      return cache.match(event.request).then((cachedResponse) => {
+        // Return a cached response if we have one
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        else {
+          return fetch(event.request).then((fetchedResponse) => {
+            // Add the network response to the cache for later visits
+            cache.put(event.request, fetchedResponse.clone());
+
+            // Return the network response
+            return fetchedResponse;
+          });
+        }
+      });
+    }));
   }
 });
